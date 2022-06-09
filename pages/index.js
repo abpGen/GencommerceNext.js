4d1d27d02bf250037a6b27debd954f515621efd4
abpGen <andres.bolivar@gentrop.com>
2022-06-09 11:30:36 -0300

    Testdrive

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Ne123123xt.js!</a>
+          Welcome to <a href="https://nextjs.org">Ne123123123xt.js!</a>
         </h1>
 
         <p className={styles.description}>
